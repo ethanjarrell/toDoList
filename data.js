bd410f1985fabf42b6bf97e3f7d624b88db9459c
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-06-24 10:09:49 -0400

    next commit

--- data.js
+++ data.js
@@ -1,11 +1,2 @@
 module.exports = {
-  toDoList: [
-  {"listItem":"Have a baby", "status": "complete"},
-
-  {"listItem":"Have another baby", "status": "complete"},
-
-  {"listItem":"Have a third baby", "status": "complete"},
-
-  {"listItem":"Have a fourth baby", "status": "complete"}
-
-]}
+  toDoList: [ ]}
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-Parser');
 const path = require('path');
+const expressValidator = require('express-validator');
 const mustacheExpress = require('mustache-express');
-
+// const jsonfile = require('jsonfile');
 const data = require("./data.js");
 const app = express();
 
@@ -13,17 +14,49 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.use(express.static('public'));
 app.set('view engine', 'mustache');
+app.use(expressValidator());
+
+// read from the json file.
+// var file = 'data.json';
+// let todo;
+// jsonfile.readFile(file, function(err, obj) {
+//   todo = obj;
+// })
+
 
 
 app.get('/', function (req, res){
 
-  res.render('index', { toDoList: data.toDoList })
+
+    // let inputResluts = input.value;
+    // let inputResults = {  },
+    // todo.toDoList.push(inputResults);
+  res.render('index', { toDoList: data.toDoList });
 });
 
 
 app.post("/", function (req, res) {
-  toDoList.push(req.body.toDoList);
+
+
+  // req.checkBody('item', 'Vader Input').notEmpty();
+  // let errors = req.validationErrors();
+  // if (errors) {
+  //   res.render('index', {errors: errors});
+  // }else{
+  //   let toDoList = {
+  //     'item' : req.body.toDoInput,
+  //     'priority' : req.body,
+  //     'checked' : ""
+  //   }
+  // }
+
+  data.toDoList.push(req.body.toDoInput);
+
+  // write to the json file.
+  // jsonfile.writeFile(file, todo);
+
   res.redirect('/');
+  
 });
 
 app.listen(3000, function () {
